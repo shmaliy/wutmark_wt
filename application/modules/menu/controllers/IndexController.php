94a8bb755f4e7d6578d50eb9dac1e4c69193eb80
maxim <shmaliy.maxim@gmail.com>
2012-10-16 23:58:24 +0300

    123

--- application/modules/menu/controllers/IndexController.php
+++ application/modules/menu/controllers/IndexController.php
@@ -17,6 +17,32 @@ class Menu_IndexController extends Zend_Controller_Action
  		$params = $request->getParams();
  		
  		$items = $this->_model->getMenuItems($params['rootAlias'], 'topMenu', '60');
+ 		foreach ($items as &$item) {
+ 			if ($item['title_alias'] == 'production') {
+ 				$item['childs'] = array();
+ 				$root = $this->_model->getRootCategoryEntryByAlias('production');
+ 				$tree = $this->_model->prodTree($root['id']);
+ 				//$this->helper->arrayTrans($tree);
+ 				foreach ($tree as $branch) {
+ 					if (!empty($branch['childs']) && isset($branch['childs'])) {
+ 						$subsubmenu = array();
+ 						foreach ($branch['childs'] as $child) {
+ 							$subsubmenu[] = array(
+ 							'title' => $child['title'],
+ 							'link' => '/' . Zend_Registry::get('lang') . '/production/' . $branch['title_alias'] . '/' . $child['title_alias'],
+ 							);
+ 						}
+ 					}
+ 					
+ 					$item['childs'][] = array(
+ 						'title' => $branch['title'],
+ 						'link' => '/' . Zend_Registry::get('lang') . '/production/' . $branch['title_alias'],
+ 						'childs' => $subsubmenu
+ 					);
+ 				}
+ 			}
+ 		}
+ 		
  		//$this->helper->arrayTrans($items);
  		$this->view->items = $items;
  	}
--- application/modules/menu/models/MenuFrontend.php
+++ application/modules/menu/models/MenuFrontend.php
@@ -12,4 +12,11 @@ class Menu_Model_MenuFrontend extends My_Model_Abstract
 	{
 		return $this->_getMenuTree($this->_getMenuItems(), $this->_getMenuItemIdByAlias($alias));
 	}
+	
+	public function prodTree($parent)
+	{
+		$items = $this->_getCategoriesItems();
+		$tree = $this->_getCategoriesTree($items, $parent);
+		return ($tree);
+	}
 }
